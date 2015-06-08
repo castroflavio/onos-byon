c663ed51f7706544a35dac0fcb9a62d9abbe4ee1
Flavio Castro <castro.flaviojr@gmail.com>
2015-06-08 14:21:25 -0700

    new file:   byon-app/src/main/java/org/onos/byon/NetworkEvent.java 	new file:   byon-app/src/main/java/org/onos/byon/NetworkListener 	new file:   byon-app/src/main/java/org/onos/byon/NetworkStoreDelegate.java 	modified:   byon-app/src/main/java/org/onos/byon/NetworkManager.java 	modified:   byon-app/src/main/java/org/onos/byon/NetworkService.java 	modified:   byon-app/src/main/java/org/onos/byon/NetworkStore.java 	modified:   byon-app/src/main/java/org/onos/byon/SimpleNetworkStore.java 	modified:   byon-cli/pom.xml 	modified:   startmn.sh

---
+++ byon-app/src/main/java/org/onos/byon/NetworkEvent.java
@@ -0,0 +1,17 @@
+package org.onos.byon;
+
+import org.onosproject.event.AbstractEvent;
+
+public class NetworkEvent extends AbstractEvent<NetworkEvent.Type, String> {
+
+    enum Type {
+        NETWORK_ADDED,
+        NETWORK_REMOVED,
+        NETWORK_UPDATED
+    }
+
+    public NetworkEvent(Type type, String subject) {
+        super(type, subject);
+    }
+
+}
---
+++ byon-app/src/main/java/org/onos/byon/NetworkStoreDelegate.java
@@ -0,0 +1,5 @@
+package org.onos.byon;
+
+import org.onosproject.store.StoreDelegate;
+
+public interface NetworkStoreDelegate extends StoreDelegate<NetworkEvent> {}
